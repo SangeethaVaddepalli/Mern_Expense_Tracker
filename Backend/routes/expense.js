ad65b642f40a4a6147e581f76fddd9ba877a6a7a
Sangeetha Vaddepalli <140165326+SangeethaVaddepalli@users.noreply.github.com>
2025-02-04 21:34:32 +0530

    Update expense.js

--- Backend/routes/expense.js
+++ Backend/routes/expense.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const router = express.Router(); //calling Router fun from express
-const Expense = require("../models/Expense"); //
+const router = express.Router();
+const Expense = require("../models/Expense"); 
 
 // add expense to database
-router.post("/",async (req,res)=>{ // using mthd post to add expense and callback fun how to dislay expense on databse
+router.post("/",async (req,res)=>{ 
     try{
         const newExpense = await Expense(req.body);
         const expense = newExpense.save();
@@ -12,15 +12,12 @@ router.post("/",async (req,res)=>{ // using mthd post to add expense and callbac
     }catch(cerror){
         res.status(500).json(error);
     }
-
 });
-
-// Get all expenses
-router.get("/",async (req,res)=>{  // using mthd get to fetch data from router/databse
+router.get("/",async (req,res)=>{  
     console.log(req.body)
     try{ 
-        const expenses = await Expense.find().sort({createdAt:-1});// sort to display latest exp at top
-        res.status(200).json(expenses);//sending exp to frontend to display on interface
+        const expenses = await Expense.find().sort({createdAt:-1});
+        res.status(200).json(expenses);
 
 
     }catch(error){
@@ -54,10 +51,6 @@ router.delete("/:id",async (req,res)=>{
 
     }catch(error){
         res.status(500).json(error);
-
     }
-
 });
-
-// export router bcoz we will use in index.js for our entry file to our server
 module.exports = router;
